3dc5e8cc6195aadfd92b7e5000af0947610b6ead
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-01-26 10:50:52 +0300

    Update 3kyu_make_a_spiral.py

--- Solutions/3kyu_make_a_spiral.py
+++ Solutions/3kyu_make_a_spiral.py
@@ -12,26 +12,20 @@ def spiralize(size):
           # up horiz
           hup = sp[upper_shift]
           for i in range(side_shift, size-1):
-            hup[i] = 0
-            sp[upper_shift] = hup
+            hup[i] = 0; sp[upper_shift] = hup
 
           # right vert
           for i in range(upper_shift, size):
-            rvt = sp[i]
-            rvt[size-2] = 0
-            sp[i] = rvt
+            rvt = sp[i]; rvt[size-2] = 0; sp[i] = rvt
           
           # bottom horiz
           hbt = sp[size-1]
           for i in range(side_shift+1, size-1):
-            hbt[i] = 0
-            sp[size-1] = hbt
+            hbt[i] = 0; sp[size-1] = hbt
           
           # left vert
           for i in range(upper_shift+2, size):
-            vlt = sp[i]
-            vlt[side_shift+1] = 0
-            sp[i] = vlt
+            vlt = sp[i]; vlt[side_shift+1] = 0; sp[i] = vlt
           
           upper_shift += 2
           side_shift  += 2
