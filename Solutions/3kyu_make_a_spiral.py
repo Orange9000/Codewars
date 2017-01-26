0ad1cf7c89dc075438506e3c38813fb0f1a994a4
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-01-26 10:49:32 +0300

    Update 3kyu_make_a_spiral.py

--- Solutions/3kyu_make_a_spiral.py
+++ Solutions/3kyu_make_a_spiral.py
@@ -1,11 +1,11 @@
 def spiralize(size):
     
     # create 2d array in a form of an enumerated dict (starting at 1)
-    sp = dict(zip(list(range(1, size+1)), ([1] * size for x in range(1,size+1))))
+    sp = dict(zip(list(range(1, size+1)), ([1]*size for x in range(1, size+1))))
     
     # reference values
     upper_shift = 2
-    side_shift = 0
+    side_shift  = 0
     
     while True:
       try:
@@ -13,7 +13,7 @@ def spiralize(size):
           hup = sp[upper_shift]
           for i in range(side_shift, size-1):
             hup[i] = 0
-          sp[upper_shift] = hup
+            sp[upper_shift] = hup
 
           # right vert
           for i in range(upper_shift, size):
@@ -25,16 +25,16 @@ def spiralize(size):
           hbt = sp[size-1]
           for i in range(side_shift+1, size-1):
             hbt[i] = 0
-          sp[size-1] = hbt
+            sp[size-1] = hbt
           
           # left vert
           for i in range(upper_shift+2, size):
             vlt = sp[i]
             vlt[side_shift+1] = 0
             sp[i] = vlt
-            
-          size  -= 2
+          
           upper_shift += 2
-          side_shift += 2
+          side_shift  += 2
+          size        -= 2
         
       except: return [sp[x] for x in sorted(sp.keys())]
