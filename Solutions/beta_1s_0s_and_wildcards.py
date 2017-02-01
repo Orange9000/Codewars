979630d9bb7b712a72a1600791badabf26f72bcd
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-02-01 08:40:39 +0300

    Delete beta_1s_0s_and_wildcards.py

--- Solutions/beta_1s_0s_and_wildcards.py
+++
@@ -1,5 +0,0 @@
-from itertools import combinations
-from functools import reduce
-
-possibilities = lambda a: [check(x) for x in combinations('01', len(a))]
-check = lambda a,b: a if all([i==j for i,j in zip(a,b) if j!='?']) else False
