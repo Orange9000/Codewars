60bdc7e4309597c637facbf6975596aa6faa359e
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-02-10 15:31:05 +0300

    Update 4kyu_sudoku_solution_validator.py

--- Solutions/4kyu/4kyu_sudoku_solution_validator.py
+++ Solutions/4kyu/4kyu_sudoku_solution_validator.py
@@ -3,7 +3,6 @@ from functools import reduce
 compare = [1,2,3,4,5,6,7,8,9]
 
 def validSolution(board):
-    
     for i in board:
         if sorted(i) == compare: continue
         else: return False
@@ -13,12 +12,12 @@ def validSolution(board):
         else: return False
     
     for j in range(0,9,3):
-    	if checker([board[j][:3],board[j+1][:3],board[j+2][:3]]) and \
-    	   checker([board[j][3:6],board[j+1][3:6],board[j+2][3:6]]) and \
-    	   checker([board[j][6:],board[j+1][6:],board[j+2][6:]]):
-    		continue
-    	else:
-    		return False
+        if checker([board[j][:3],board[j+1][:3],board[j+2][:3]]) and \
+            checker([board[j][3:6],board[j+1][3:6],board[j+2][3:6]]) and \
+            checker([board[j][6:],board[j+1][6:],board[j+2][6:]]):
+            continue
+        else:
+            return False
     return True
 
 def checker(three_by_three):
