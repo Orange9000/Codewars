8ebad2d4bb48153df1558a189392531cf037f054
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-04-06 11:19:57 +0300

    Update 6kyu_numerical_palindrome2.py
    Previous solution didn't match some palindromes

--- Solutions/6kyu/6kyu_numerical_palindrome2.py
+++ Solutions/6kyu/6kyu_numerical_palindrome2.py
@@ -1,6 +1,13 @@
-import re
+from itertools import combinations_with_replacement as cwr
 
 def palindrome(num):
     if not isinstance(num, int) or num < 0: return "Not valid"
     num = str(num)
-    return bool(re.findall(r'(\d)\1', num)) or num == num[::-1] and len(num)>1
+    return search(num) if len(num) > 1 else False
+
+
+def search(s):
+    for i,j in cwr(range(len(s)+1), 2):
+        check = s[i:j]
+        if len(check) > 1 and check == check[::-1]: return True
+    return False
