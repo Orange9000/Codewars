b74d1c4a0b8e033a58a870a05bc4fc16763b8983
Orange9000 <rollingonlinoleumfarting@gmail.com>
2017-06-06 13:51:58 +0300

    Create 5kyu_evil_programming_government_restrictions.js

---
+++ Solutions/5kyu/5kyu_evil_programming_government_restrictions.js
@@ -0,0 +1,5 @@
+var anarchy = function () {
+  return 'I can write numbers like, ' + [null,null,null,null].map((e,i)=>i)
+                                                             .filter(e=>e!=false)
+                                                             .join(', ') +'.';
+}
